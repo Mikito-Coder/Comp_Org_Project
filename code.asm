f0973021b570d8af297d37506e371bcfc22e4a89
Mikito-Coder <Ayandosu10@gmail.com>
2023-11-27 15:33:54 -0700

    Update code.asm
    This part of my code stores the prompt in my register and prints it to my console.

--- code.asm
+++ code.asm
@@ -3,3 +3,8 @@
     output: .asciiz "\nYou typed the number "
     illegal: .asciiz "illegal Number!\n"
     newline: .asciiz "\n"
+.text
+main:
+    li $v0, 4          # syscall: print_str
+    la $a0, prompt     # load the address of the prompt string
+    syscall            # make the system call to print the prompt
