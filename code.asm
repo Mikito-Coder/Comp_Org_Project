0946ea43293723c56c131030f474365433f15245
Mikito-Coder <Ayandosu10@gmail.com>
2023-11-27 15:37:30 -0700

    Update code.asm
    Prompts the user to enter an integer and stores the result in register $v0.

--- code.asm
+++ code.asm
@@ -8,3 +8,7 @@ main:
     li $v0, 4          # syscall: print_str
     la $a0, prompt     # load the address of the prompt string
     syscall            # make the system call to print the prompt
+    
+    li $v0, 5
+    syscall
+    move $t0, $v0
